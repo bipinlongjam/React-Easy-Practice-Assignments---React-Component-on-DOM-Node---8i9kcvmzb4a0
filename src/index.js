b6895b6c97f47a360ea35c5bb9a610e1a6a803fd
bipinlongjam <41874001+bipinlongjam@users.noreply.github.com>
2022-04-27 21:24:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
 );
